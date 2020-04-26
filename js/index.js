0808cf8df96ca89af21821e8295d7c51791a1f6e
PatheticMustan <patheticmustan@gmail.com>
2020-04-25 23:23:20 -0400

    aw yea

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
 function getDogImage() {
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
-
+        .then(responseJson => {
+            console.log(responseJson);  
+            let imageLink = responseJson.message;
+            $(".dog-img").attr("src", imageLink);
+            console.log($("body").attr("style"));
+        })
         .catch(error => alert('Something went wrong. Try again later.'));
-}
-
-function displayResults(responseJson) {
-    console.log(responseJson);
-    let arrayOfImg = responseJson.message;
-    let display = getImages(arrayOfImg); //replace the existing image with the new one
-    $('.dog-img').html(display);
-}
-
-/** * Prepares a string of <img> elements * and returns the same */
-function getImages(img){
-    return `<img src="${img}" class="dog-img">`;
 }
 
 getDogImage()
 
-$(".dog-container").click(() => getDogImage())
+$(".dog-img").click(() => getDogImage())
